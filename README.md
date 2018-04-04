be1e111e34751309dc3eb652dfd89f5c3a0ad8b2
Bruno Pacheco <brunopacheco1@yahoo.com>
2018-04-04 19:18:20 +0200

    Including geospatial tests.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # learning-elasticsearch
 Reading and Learning Elastic Search documentation and applying it on Java, Node.js and Postman.
+
+## Running Elastic Search
+
+docker run -p 9200:9200 -p 9300:9300 --rm --name elasticsearch -d -e "discovery.type=single-node" -e "cluster.name=docker-cluster" docker.elastic.co/elasticsearch/elasticsearch:6.2.3
--- elasticsearch-java/src/test/java/com/dev/bruno/learning/elasticsearch/GeospatialSearchTest.java
+++ elasticsearch-java/src/test/java/com/dev/bruno/learning/elasticsearch/GeospatialSearchTest.java
@@ -1,6 +1,7 @@
 package com.dev.bruno.learning.elasticsearch;
 
 import com.google.gson.Gson;
+import com.vividsolutions.jts.geom.Coordinate;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpHost;
 import org.apache.http.entity.ContentType;
@@ -9,13 +10,20 @@ import org.elasticsearch.action.admin.indices.create.CreateIndexRequest;
 import org.elasticsearch.action.admin.indices.create.CreateIndexResponse;
 import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequest;
 import org.elasticsearch.action.admin.indices.delete.DeleteIndexResponse;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.action.search.SearchRequest;
 import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.client.Response;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.common.geo.ShapeRelation;
+import org.elasticsearch.common.geo.builders.EnvelopeBuilder;
+import org.elasticsearch.common.geo.builders.ShapeBuilders;
 import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.xcontent.XContentType;
 import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.rest.RestStatus;
 import org.elasticsearch.search.aggregations.AggregationBuilders;
 import org.elasticsearch.search.aggregations.bucket.terms.ParsedStringTerms;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
@@ -45,7 +53,7 @@ public class GeospatialSearchTest {
     public static void setUp() {
         highLevelClient = new RestHighLevelClient(
             RestClient.builder(
-                new HttpHost("localhost", 9200, "http")
+                new HttpHost("192.168.99.100", 9200, "http")
             )
         );
 
@@ -59,11 +67,13 @@ public class GeospatialSearchTest {
 
     @Test
     public void test1CreateIndex() throws IOException {
-        CreateIndexRequest request = new CreateIndexRequest("bank");
+        CreateIndexRequest request = new CreateIndexRequest("example");
         request.settings(Settings.builder()
             .put("index.number_of_shards", 5)
             .put("index.number_of_replicas", 0)
         );
+
+        request.mapping("_doc", "location", "type=geo_shape");
 
         CreateIndexResponse createIndexResponse = highLevelClient.indices().create(request);
 
@@ -72,85 +82,40 @@ public class GeospatialSearchTest {
     }
 
     @Test
-    public void test2CreateDocuments() throws IOException, InterruptedException {
-        InputStream in = new URL( "https://raw.githubusercontent.com/elastic/elasticsearch/master/docs/src/test/resources/accounts.json" ).openStream();
+    public void test2CreateDocument() throws IOException, InterruptedException {
+        IndexRequest request = new IndexRequest(
+                "example",
+                "_doc",
+                "1");
 
-        String lowLevelRequestBody = readString(in) + "\n";
+        String requestBody = "{\n" +
+                "    \"name\": \"Wind & Wetter, Berlin, Germany\",\n" +
+                "    \"location\": {\n" +
+                "        \"type\": \"point\",\n" +
+                "        \"coordinates\": [13.400544, 52.530286]\n" +
+                "    }\n" +
+                "}";
 
-        HttpEntity entity = new NStringEntity(lowLevelRequestBody, ContentType.APPLICATION_JSON);
-        Map<String, String> params = Collections.singletonMap("pretty", "true");
+        request.source(requestBody, XContentType.JSON);
+        request.create(true);
 
-        Response response = lowLevelClient.performRequest("POST", "/bank/_doc/_bulk", params, entity);
-        Map<String, Object> responseBody = readBody(response.getEntity().getContent());
+        IndexResponse indexResponse = highLevelClient.index(request);
 
-        assert response.getStatusLine().getStatusCode() == 200;
-        assert !((Boolean) responseBody.get("errors"));
+        assert indexResponse.status().equals(RestStatus.CREATED);
 
         //Bulk is a async request
-        Thread.sleep(5000L);
+        Thread.sleep(1000L);
     }
 
     @Test
-    public void test3FirstSearch() throws IOException {
-        Response response = lowLevelClient.performRequest("GET", "/bank/_search?q=*&sort=account_number:asc&pretty");
-        Map<String, Object> responseBody = readBody(response.getEntity().getContent());
+    public void test3GeoQuery() throws IOException {
+        EnvelopeBuilder envelopeBuilder = new EnvelopeBuilder(new Coordinate(13.0, 53.0) , new Coordinate(14.0, 52.0 ));
 
-        assert response.getStatusLine().getStatusCode() == 200;
-
-        Map<String, Object> hits = (Map<String, Object>) responseBody.get("hits");
-        Double total = (Double) hits.get("total");
-        assert total == 1000;
-    }
-
-    @Test
-    public void test4QueryLanguage() throws IOException {
-        SearchSourceBuilder sourceBuilder = new SearchSourceBuilder();
-        sourceBuilder.query(
-            QueryBuilders.boolQuery()
-                .must(QueryBuilders.matchQuery("age", "40"))
-                .mustNot(QueryBuilders.matchQuery("state", "ID"))
-        );
-
-        SearchRequest searchRequest = new SearchRequest();
-        searchRequest.source(sourceBuilder);
-
-        SearchResponse searchResponse = highLevelClient.search(searchRequest);
-
-        assert searchResponse.getHits().getTotalHits() == 43;
-
-        String lowLevelRequestBody = "{\n" +
-            "  \"query\": {\n" +
-            "    \"bool\": {\n" +
-            "      \"must\": [\n" +
-            "        { \"match\": { \"age\": \"40\" } }\n" +
-            "      ],\n" +
-            "      \"must_not\": [\n" +
-            "        { \"match\": { \"state\": \"ID\" } }\n" +
-            "      ]\n" +
-            "    }\n" +
-            "  }\n" +
-            "}";
-
-        HttpEntity entity = new NStringEntity(lowLevelRequestBody, ContentType.APPLICATION_JSON);
-        Map<String, String> params = Collections.singletonMap("pretty", "true");
-
-        Response response = lowLevelClient.performRequest("GET", "/bank/_search", params, entity);
-        Map<String, Object> responseBody = readBody(response.getEntity().getContent());
-
-        assert response.getStatusLine().getStatusCode() == 200;
-
-        Map<String, Object> hits = (Map<String, Object>) responseBody.get("hits");
-        Double total = (Double) hits.get("total");
-        assert total == 43;
-    }
-
-    @Test
-    public void test5Filtering() throws IOException {
         SearchSourceBuilder sourceBuilder = new SearchSourceBuilder();
         sourceBuilder.query(
             QueryBuilders.boolQuery()
                 .must(QueryBuilders.matchAllQuery())
-                .filter(QueryBuilders.rangeQuery("balance").gte(20000).lte(30000))
+                .filter(QueryBuilders.geoShapeQuery("location", envelopeBuilder).relation(ShapeRelation.WITHIN))
         );
 
         SearchRequest searchRequest = new SearchRequest();
@@ -158,85 +123,45 @@ public class GeospatialSearchTest {
 
         SearchResponse searchResponse = highLevelClient.search(searchRequest);
 
-        assert searchResponse.getHits().getTotalHits() == 217;
+        assert searchResponse.getHits().getTotalHits() == 1;
 
         String lowLevelRequestBody = "{\n" +
-            "  \"query\": {\n" +
-            "    \"bool\": {\n" +
-            "      \"must\": { \"match_all\": {} },\n" +
-            "      \"filter\": {\n" +
-            "        \"range\": {\n" +
-            "          \"balance\": {\n" +
-            "            \"gte\": 20000,\n" +
-            "            \"lte\": 30000\n" +
-            "          }\n" +
-            "        }\n" +
-            "      }\n" +
-            "    }\n" +
-            "  }\n" +
-            "}";
+                "    \"query\":{\n" +
+                "        \"bool\": {\n" +
+                "            \"must\": {\n" +
+                "                \"match_all\": {}\n" +
+                "            },\n" +
+                "            \"filter\": {\n" +
+                "                \"geo_shape\": {\n" +
+                "                    \"location\": {\n" +
+                "                        \"shape\": {\n" +
+                "                            \"type\": \"envelope\",\n" +
+                "                            \"coordinates\" : [[13.0, 53.0], [14.0, 52.0]]\n" +
+                "                        },\n" +
+                "                        \"relation\": \"within\"\n" +
+                "                    }\n" +
+                "                }\n" +
+                "            }\n" +
+                "        }\n" +
+                "    }\n" +
+                "}";
 
         HttpEntity entity = new NStringEntity(lowLevelRequestBody, ContentType.APPLICATION_JSON);
         Map<String, String> params = Collections.singletonMap("pretty", "true");
 
-        Response response = lowLevelClient.performRequest("GET", "/bank/_search", params, entity);
+        Response response = lowLevelClient.performRequest("GET", "/example/_search", params, entity);
         Map<String, Object> responseBody = readBody(response.getEntity().getContent());
 
         assert response.getStatusLine().getStatusCode() == 200;
 
         Map<String, Object> hits = (Map<String, Object>) responseBody.get("hits");
         Double total = (Double) hits.get("total");
-        assert total == 217;
-    }
-
-
-
-    @Test
-    public void test6Aggregations() throws IOException {
-        SearchSourceBuilder sourceBuilder = new SearchSourceBuilder();
-        sourceBuilder.size(0)
-            .aggregation(AggregationBuilders.terms("group_by_state").field("state.keyword"));
-
-        SearchRequest searchRequest = new SearchRequest();
-        searchRequest.source(sourceBuilder);
-
-        SearchResponse searchResponse = highLevelClient.search(searchRequest);
-
-        ParsedStringTerms groupByState = (ParsedStringTerms) searchResponse.getAggregations().get("group_by_state");
-
-        assert groupByState.getDocCountError() == 20;
-        assert groupByState.getSumOfOtherDocCounts() == 770;
-        
-        String lowLevelRequestBody = "{\n" +
-            "  \"size\": 0,\n" +
-            "  \"aggs\": {\n" +
-            "    \"group_by_state\": {\n" +
-            "      \"terms\": {\n" +
-            "        \"field\": \"state.keyword\"\n" +
-            "      }\n" +
-            "    }\n" +
-            "  }\n" +
-            "}";
-
-        HttpEntity entity = new NStringEntity(lowLevelRequestBody, ContentType.APPLICATION_JSON);
-        Map<String, String> params = Collections.singletonMap("pretty", "true");
-
-        Response response = lowLevelClient.performRequest("GET", "/bank/_search", params, entity);
-        Map<String, Object> responseBody = readBody(response.getEntity().getContent());
-
-        assert response.getStatusLine().getStatusCode() == 200;
-
-        Map<String, Object> aggregations = (Map<String, Object>) responseBody.get("aggregations");
-        Map<String, Object> byStates = (Map<String, Object>) aggregations.get("group_by_state");
-        Double docCountErrorUpperBound = (Double) byStates.get("doc_count_error_upper_bound");
-        Double sumOtherDocCount = (Double) byStates.get("sum_other_doc_count");
-        assert docCountErrorUpperBound == 20;
-        assert sumOtherDocCount == 770;
+        assert total == 1;
     }
 
     @Test
     public void test7DeleteIndex() throws IOException {
-        DeleteIndexRequest request = new DeleteIndexRequest("bank");
+        DeleteIndexRequest request = new DeleteIndexRequest("example");
 
         //It is possible to do in a async request, but for test it is not necessary.
         DeleteIndexResponse deleteIndexResponse = highLevelClient.indices().delete(request);
